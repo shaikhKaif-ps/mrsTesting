7a087adba216f7d6a50e211b92cd0c61bf217f3a
sk-kaif <kaif.s@parashifttech.com>
2025-11-13 11:33:07 +0530

    schema import issue

--- src/seo/blogSchemas/index.js
+++ src/seo/blogSchemas/index.js
@@ -1,6 +1,5 @@
 //src/seo/blogSchemas/index.js
-import { railLogisticsRenewableSchema } from "./RailLogisticsforRenewable";
-// import others...
+import { railLogisticsRenewableSchema } from "./RailLogisticsforRenewable.js";
 
 export const blogSchemas = {
   "rail-logistics-for-renewable": railLogisticsRenewableSchema,
